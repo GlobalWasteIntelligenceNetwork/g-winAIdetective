80b8e9a55b660937fe720c4a1e3b5c8c92b01d70
GlobalWasteIntelligenceNetwork <globalwasteintelligencenetwork@gmail.com>
2026-08-26 05:56:58 -0400

    Add COUNTRIES and ENVIRONMENTS constants

---
+++ src/data/options.ts
@@ -0,0 +1,21 @@
+import type { Country, EnvironmentType } from '@/types';
+
+export const COUNTRIES: Country[] = [
+  'Canada',
+  'Spain',
+  'China',
+  'United States',
+  'France',
+  'Germany',
+  'Other',
+];
+
+export const ENVIRONMENTS: EnvironmentType[] = [
+  'Park',
+  'Street',
+  'School',
+  'Beach',
+  'River / waterway',
+  'Residential area',
+  'Other',
+];
